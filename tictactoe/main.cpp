051d60e7b931c9b6866989df8b928affca042dd1
Sujal Singh <email.sujalsingh@gmail.com>
2025-11-13 17:31:20 +0530

    tictactoe-cpp: read and validate player moves from input

--- tictactoe/main.cpp
+++ tictactoe/main.cpp
@@ -11,6 +11,33 @@ class TicTacToe {
         {' ', ' ', ' '}
     };
 
+    std::pair<int, int> getMove(char player) {
+        int row, col;
+        char comma;
+
+        while (true) {
+            std::cout << "Move for player '" << player << "': ";
+
+            std::cin >> row >> comma >> col;
+            std::cout << "\n";
+
+            if (row < 0 || row > 2 || col < 0 || col > 2) {
+                std::cout << "Out of bounds move, please pick a valid cell.\n";
+                continue;
+            }
+
+            if (board[row][col] != ' ') {
+                std::cout << "Invalid move, cell is already occupied.\n";
+                continue;
+            }
+
+            board[row][col] = player;
+            break;
+        }
+
+        return {row, col};
+    }
+
 public:
     void printBoard() const {
         std::cout
@@ -29,11 +56,20 @@ public:
                 << " │ " << board[2][2] << " │\n"
                 << "  └───┴───┴───┘\n\n";
     }
+
+    void run() {
+        while (true) {
+            printBoard();
+            getMove(x);
+            printBoard();
+            getMove(o);
+        }
+    }
 };
 
 
 int main() {
     TicTacToe game;
-    game.printBoard();
+    game.run();
     return 0;
 }
