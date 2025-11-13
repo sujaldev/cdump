5ec9df0fa8fadb5a7f00c8b254d48962d276dada
Sujal Singh <email.sujalsingh@gmail.com>
2025-11-13 17:25:37 +0530

    tictactoe-cpp: init class `TicTacToe()`

--- tictactoe/main.cpp
+++ tictactoe/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <vector>
+
+using Board = std::vector<std::vector<char> >;
+constexpr char x = 'x', o = 'o';
+
+class TicTacToe {
+    Board board = {
+        {' ', ' ', ' '},
+        {' ', ' ', ' '},
+        {' ', ' ', ' '}
+    };
+};
 
 
 int main() {
+    TicTacToe game;
     return 0;
 }
