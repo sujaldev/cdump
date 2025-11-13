748defab70b862e53736019f8554e557b90f342a
Sujal Singh <email.sujalsingh@gmail.com>
2025-11-13 17:27:16 +0530

    tictactoe-cpp: implement method to print board

--- tictactoe/main.cpp
+++ tictactoe/main.cpp
@@ -10,10 +10,30 @@ class TicTacToe {
         {' ', ' ', ' '},
         {' ', ' ', ' '}
     };
+
+public:
+    void printBoard() const {
+        std::cout
+                << "    0   1   2\n"
+                << "  ┌───┬───┬───┐\n"
+                << "0 │ " << board[0][0]
+                << " │ " << board[0][1]
+                << " │ " << board[0][2] << " │\n"
+                << "  ├───┼───┼───┤\n"
+                << "1 │ " << board[1][0]
+                << " │ " << board[1][1]
+                << " │ " << board[1][2] << " │\n"
+                << "  ├───┼───┼───┤\n"
+                << "2 │ " << board[2][0]
+                << " │ " << board[2][1]
+                << " │ " << board[2][2] << " │\n"
+                << "  └───┴───┴───┘\n\n";
+    }
 };
 
 
 int main() {
     TicTacToe game;
+    game.printBoard();
     return 0;
 }
